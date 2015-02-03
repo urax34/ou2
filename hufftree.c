d0478a31fc96636c1ed42e1770982b73996d7499
urax34 <stefanbranberg@gmail.com>
2015-02-03 07:53:16 +0100

    Nu funkar det ännu bättre! Jag hade glömt parent-pekaren.... Forfarande minnesläckor men på bättringsvägen!

--- hufftree.c
+++ hufftree.c
@@ -81,98 +81,82 @@ huffTree_pos huffTree_insertLeft(huff_tree *tree,huffTree_pos n) {
     n->leftChild=p;
     return p;
 }
-
 huff_tree *huffTree_merge (huff_tree *treeLeft, huff_tree *treeRight){
     huff_tree *newTree=huffTree_create();
+    //huffTree_pos r = huffTree_root(newTree);
+    newTree->root->leftChild=treeLeft->root;
+    treeLeft->root->parent=newTree->root;
+    newTree->root->rightChild=treeRight->root;
+    treeRight->root->parent=newTree->root;
 
-    data CharLeftData, CharRightData, WeightLeftData, WeightRightData;
-//    int tempCharLeft, tempCharRight, tempWeightLeft, tempWeightRight;
-    huffTree_pos r = huffTree_root(newTree);
 
+    data WeightLeftData, WeightRightData;
     huffTree_pos left = huffTree_root(treeLeft);
-    r = huffTree_insertLeft(newTree,r);
-    //tempCharLeft = huffTree_inspectCharacter(treeLeft, left);
-    CharLeftData = huffTree_inspectCharacter(treeLeft, left);
-    //tempWeightLeft = huffTree_inspectWeight(treeLeft, left);
-    WeightLeftData = huffTree_inspectWeight(treeLeft, left);
-    //huffTree_setValues(newTree,tempCharLeft,tempWeightLeft,r);
-    huffTree_setValues(newTree,CharLeftData,WeightLeftData,r);
-
-    if (huffTree_hasLeftChild(treeLeft,left)){
-        r->leftChild=left->leftChild;
-    }
-    if (huffTree_hasRightChild(treeLeft,left)){
-        r->rightChild=left->rightChild;
-    }
-
-    r=huffTree_root(newTree);
     huffTree_pos right = huffTree_root(treeRight);
-    r = huffTree_insertRight(newTree,r);
-    //tempCharRight = huffTree_inspectCharacter(treeRight, right);
-    CharRightData = huffTree_inspectCharacter(treeRight, right);
-    //tempWeightRight = huffTree_inspectWeight(treeRight, right);
+    WeightLeftData = huffTree_inspectWeight(treeLeft, left);
     WeightRightData = huffTree_inspectWeight(treeRight, right);
-    //huffTree_setValues(newTree,tempCharRight,tempWeightRight,r);
-    huffTree_setValues(newTree,CharRightData,WeightRightData,r);
-
-    if (huffTree_hasLeftChild(treeRight,right)){
-        r->leftChild=right->leftChild;
-    }
-    if (huffTree_hasRightChild(treeRight,right)){
-        r->rightChild=right->rightChild;
-    }
-    r=huffTree_root(newTree);
+    huffTree_pos r=huffTree_root(newTree);
     int sum = (int)(intptr_t)WeightLeftData+(int)(intptr_t)WeightRightData;
     data sumData = (data)(intptr_t)sum;
-    printf("\n Data  = %d\n", (int)sum);
-    //huffTree_setValues(newTree,0,(int)tempWeightLeft + (int)tempWeightRight,r);
+
     huffTree_setValues(newTree,NULL,sumData,r);
-    //huffTree_free(treeLeft);
-    //huffTree_free(treeRight);
-    //huffTree_deleteNode(treeLeft,huffTree_root(treeLeft));
-    //free(treeLeft->root->character);
-    //free(treeLeft->root->weight);
-
-    //treeLeft->root->leftChild=NULL;
-    //treeRight->root->leftChild=NULL;
-//    if (treeLeft->root->character=!NULL)
-//        free(treeLeft->root->character);
-
-//    if (treeLeft->root->weight=!NULL)
-//        free(treeLeft->root->weight);
-   // huffTree_free(treeLeft);
 
 
-    if (treeLeft->root!=NULL)
-        free(treeLeft->root);
-    if (treeLeft->root!=NULL)
-        free(treeRight->root);
-    //free(treeRight);
-    free(treeLeft);
-    free(treeRight);
+    treeLeft->root=NULL;
+    treeRight->root=NULL;
     return newTree;
-   /* data tempChar, tempWeightLeft, tempWeightRight;
-    huffTree_pos p = huffTree_root(treeLeft);
-    tempChar = huffTree_inspectCharacter(treeLeft, p);
-    tempWeightLeft = huffTree_inspectWeight(treeLeft, p);
-    p = huffTree_insertLeft(treeLeft,p);
-    huffTree_setValues(treeLeft,tempChar,tempWeightLeft,p);
-
-    p = huffTree_root(treeLeft);
-
-    p = huffTree_insertRight(treeLeft,p);
-
-    huffTree_pos n=huffTree_root(treeRight);
-    tempChar = huffTree_inspectCharacter(treeRight, n);
-    tempWeightRight = huffTree_inspectWeight(treeRight, n);
-
-    huffTree_setValues(treeLeft, tempChar, tempWeightRight,p);
-
-    p = huffTree_root(treeLeft);
-    huffTree_setValues(treeLeft,0,(int)tempWeightLeft+(int)tempWeightRight,p);*/
-
-//    return newTree;
 }
+
+//
+//huff_tree *huffTree_merge (huff_tree *treeLeft, huff_tree *treeRight){
+//    huff_tree *newTree=huffTree_create();
+//
+//    data CharLeftData, CharRightData, WeightLeftData, WeightRightData;
+//
+//
+//    huffTree_pos r = huffTree_root(newTree);
+//
+//    huffTree_pos left = huffTree_root(treeLeft);
+//    r = huffTree_insertLeft(newTree,r);
+//
+//    CharLeftData = huffTree_inspectCharacter(treeLeft, left);
+//
+//    WeightLeftData = huffTree_inspectWeight(treeLeft, left);
+//
+//    huffTree_setValues(newTree,CharLeftData,WeightLeftData,r);
+//
+//    if (huffTree_hasLeftChild(treeLeft,left)){
+//        r->leftChild=left->leftChild;
+//    }
+//    if (huffTree_hasRightChild(treeLeft,left)){
+//        r->rightChild=left->rightChild;
+//    }
+//
+//    r=huffTree_root(newTree);
+//    huffTree_pos right = huffTree_root(treeRight);
+//    r = huffTree_insertRight(newTree,r);
+//
+//
+//    CharRightData = huffTree_inspectCharacter(treeRight, right);
+//
+//    WeightRightData = huffTree_inspectWeight(treeRight, right);
+//
+//    huffTree_setValues(newTree,CharRightData,WeightRightData,r);
+//
+//    if (huffTree_hasLeftChild(treeRight,right)){
+//        r->leftChild=right->leftChild;
+//    }
+//    if (huffTree_hasRightChild(treeRight,right)){
+//        r->rightChild=right->rightChild;
+//    }
+//    r=huffTree_root(newTree);
+//    int sum = (int)(intptr_t)WeightLeftData+(int)(intptr_t)WeightRightData;
+//    data sumData = (data)(intptr_t)sum;
+//
+//    huffTree_setValues(newTree,NULL,sumData,r);
+//
+//    return newTree;
+//}
 
 huffTree_pos huffTree_deleteNode(huff_tree *tree,huffTree_pos n) {
     huffTree_pos parent=n->parent;
