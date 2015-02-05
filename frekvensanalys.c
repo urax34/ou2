c7f25e8ee89ada5b5edbdb353c893a79a96dc9c7
urax34 <stefanbranberg@gmail.com>
2015-02-05 12:42:15 +0100

    senaste versionen, nu med sortering efter varje mergetree

--- frekvensanalys.c
+++ frekvensanalys.c
@@ -42,7 +42,9 @@ void siftup (int i, int n, HuffIntArrayTable *h){
             h->array[i][0]=copyC;
         j=(2*i)+1;
     }
-
+//    for(int i= 0; i<h->arrNumber; i++){
+//        printf("%c %d\n", (char)h->array[i][0], h->array[i][1]);
+//    }
 }
 /*Sorts a HuffIntArrayTable by the number of times each char has appeared in a text,
     lowest first. */
@@ -72,7 +74,7 @@ void treesortHuffArr(HuffIntArrayTable *h, int n){
         h->array[0][0]=h->array[i-1][0];
         h->array[i-1][0]=temp;
 
-    printf("\n\n");
+    //printf("\n\n");
     }
 
 }
@@ -94,129 +96,123 @@ huff_tree *huffEncode (HuffIntArrayTable *harr){
         printf("%c%d ", (char)(intptr_t)huffArr[x]->root->character, (int)(intptr_t)huffArr[x]->root->weight);
     }
 
-    int i = 0;
+
     int n = 1;
-    huffTree_pos pos_i;
-    huffTree_pos pos_mid;
+
     huffTree_pos pos_n;
-   // int weight_i;
-    data weight_i;
-   // int weight_mid;
-    data weight_mid;
-   // int weight_n;
-    data weight_n;
+    data weight_t;
+    data weight_j;
 
 /*Här slås träden ihop ett och ett i ordning. Det finns antagligen minnesläckor...*/
-    while (i<n){
-        printf("\n i = %d n = %d\n", i, n);
-        pos_i = huffTree_root(huffArr[i]);
-        pos_mid = huffTree_root(huffArr[i+1]);
-        pos_n = huffTree_root(huffArr[n]);
-
-        //weight_i = (intptr_t)huffTree_inspectWeight(huffArr[i],pos_i);
+    while (n<size){
+        //printf("\n i = %d n = %d\n", i, n);
+        /*pos_i = huffTree_root(huffArr[i]);
+        pos_n = huffTree_root(huffArr[i+1]);
         weight_i = huffTree_inspectWeight(huffArr[i],pos_i);
-        //weight_mid = (intptr_t)huffTree_inspectWeight(huffArr[i+1],pos_mid);
-        weight_mid = huffTree_inspectWeight(huffArr[i+1],pos_mid);
-        //weight_n = (intptr_t)huffTree_inspectWeight(huffArr[n],pos_n);
-        weight_n = huffTree_inspectWeight(huffArr[n],pos_n);
+        weight_n = huffTree_inspectWeight(huffArr[i+1],pos_n);
+        */
 
-        if (i == 0 && n == 1){
+        huffArr[n]=huffTree_merge(huffArr[n], huffArr[n-1]);
+        int t = n;
+        int j = n+1;
 
-            huffArr[n]= huffTree_merge(huffArr[n],huffArr[i]);
-            i++;
-            if (n<size-2){
-                n=n+2;
-            }
-            else if (n<size-1){
-                n++;
-            }
+        if (j<size){
 
+            //weight_t = huffTree_inspectWeight(huffArr[t],huffTree_root(huffArr[t]));
+            //weight_j = huffTree_inspectWeight(huffArr[j],huffTree_root(huffArr[j]));
+            while (j<size && (int)(intptr_t)huffArr[t]->root->weight >(int)(intptr_t)huffArr[j]->root->weight){
+
+                weight_t = huffTree_inspectWeight(huffArr[t],huffTree_root(huffArr[t]));
+                weight_j = huffTree_inspectWeight(huffArr[j],huffTree_root(huffArr[j]));
+
+                printf("\n t = %d j = %d\n", (int)(intptr_t)weight_t, (int)(intptr_t)weight_j);
+
+                huff_tree *tempHuff = huffTree_create();
+
+
+        for(int x= 0; x<size; x++){
+            printf("%c%d ", (char)(intptr_t)huffArr[x]->root->character, (int)(intptr_t)huffArr[x]->root->weight);
         }
-        else if (i == size-2 && n == size-1){
 
-            huffArr[n]= huffTree_merge(huffArr[n],huffArr[i]);
-            i++;
-
-        }
-
-        else if ((int)(intptr_t)weight_i+(int)(intptr_t)weight_mid<=(int)(intptr_t)weight_mid+(int)(intptr_t)weight_n
-                 && (int)(intptr_t)weight_i+(int)(intptr_t)weight_mid<=(int)(intptr_t)weight_i+(int)(intptr_t)weight_n){
-            huffArr[i+1]= huffTree_merge(huffArr[i+1],huffArr[i]);
-            i++;
-            if (n<size-1){
-            n++;
-            }
-
-        }
-        else if ((int)(intptr_t)weight_i+(int)(intptr_t)weight_n<(int)(intptr_t)weight_mid+(int)(intptr_t)weight_n
-                 && (int)(intptr_t)weight_i+(int)(intptr_t)weight_n<(int)(intptr_t)weight_mid+(int)(intptr_t)weight_i){
-            huffArr[n]= huffTree_merge(huffArr[n],huffArr[i]);
-            i++;
-            if (n<size-1){
-                n++;
+        printf("\n\n");
+                tempHuff=huffArr[t];
+                huffArr[t]=huffArr[j];
+                huffArr[j]=tempHuff;
+                t++;
+                j++;
             }
         }
-        else if ((int)(intptr_t)weight_i+(int)(intptr_t)weight_mid>=(int)(intptr_t)weight_mid+(int)(intptr_t)weight_n){
-            huffArr[n]= huffTree_merge(huffArr[n],huffArr[i+1]);
+        n++;
 
-            huffArr[i+1]=huffArr[i];
 
-            i++;
-            if (n<size-2){
-                n++;
-            }
-        }
-           for(int x= 0; x<size; x++){
-       printf("%c%d ", (char)(intptr_t)huffArr[x]->root->character, (int)(intptr_t)huffArr[x]->root->weight);
-    }
-        //i++;
-    printf("\n");
     }
     /*Här skriver jag ut root-värdet, värdet på dom två första barnen och om det högra
     barnet har barn så skrivs dom också ut*/
-            printf("\n\n Root %c %d ", (char)(intptr_t)huffArr[n]->root->character, (int)(intptr_t)huffArr[n]->root->weight);
 
-            huffTree_pos p = huffTree_root(huffArr[n]);
-            p = huffTree_leftChild(huffArr[n],p);
-            data testC = huffTree_inspectCharacter(huffArr[n],p);
-            data testw = huffTree_inspectWeight(huffArr[n],p);
+            printf("\n\n Root %c %d ", (char)(intptr_t)huffArr[n-1]->root->character, (int)(intptr_t)huffArr[n-1]->root->weight);
+
+            huffTree_pos p = huffTree_root(huffArr[n-1]);
+            p = huffTree_leftChild(huffArr[n-1],p);
+            data testC = huffTree_inspectCharacter(huffArr[n-1],p);
+            data testw = huffTree_inspectWeight(huffArr[n-1],p);
             printf("Left %c %d", (int)(intptr_t)testC, (int)(intptr_t)testw);
 
-            p = huffTree_root(huffArr[n]);
-            p = huffTree_rightChild(huffArr[n],p);
-            testC = huffTree_inspectCharacter(huffArr[n],p);
-            testw = huffTree_inspectWeight(huffArr[n],p);
+            p = huffTree_root(huffArr[n-1]);
+            p = huffTree_rightChild(huffArr[n-1],p);
+            testC = huffTree_inspectCharacter(huffArr[n-1],p);
+            testw = huffTree_inspectWeight(huffArr[n-1],p);
             printf("Right %c %d", (int)(intptr_t)testC, (int)(intptr_t)testw);
             printf("\n\n Right Child \n");
 
-            if (huffTree_hasLeftChild(huffArr[n],p)){
-                p = huffTree_leftChild(huffArr[n],p);
-                data testC = huffTree_inspectCharacter(huffArr[n],p);
-                data testw = huffTree_inspectWeight(huffArr[n],p);
+            if (huffTree_hasLeftChild(huffArr[n-1],p)){
+                p = huffTree_leftChild(huffArr[n-1],p);
+                data testC = huffTree_inspectCharacter(huffArr[n-1],p);
+                data testw = huffTree_inspectWeight(huffArr[n-1],p);
                 printf("Left %c %d", (int)(intptr_t)testC, (int)(intptr_t)testw);
             }
 
-            p = huffTree_root(huffArr[n]);
-            p = huffTree_rightChild(huffArr[n],p);
+            p = huffTree_root(huffArr[n-1]);
+            p = huffTree_rightChild(huffArr[n-1],p);
 
-            if (huffTree_hasRightChild(huffArr[n],p)){
-                p = huffTree_rightChild(huffArr[n],p);
-                data testC = huffTree_inspectCharacter(huffArr[n],p);
-                data testw = huffTree_inspectWeight(huffArr[n],p);
+            if (huffTree_hasRightChild(huffArr[n-1],p)){
+                p = huffTree_rightChild(huffArr[n-1],p);
+                data testC = huffTree_inspectCharacter(huffArr[n-1],p);
+                data testw = huffTree_inspectWeight(huffArr[n-1],p);
+                printf("Right %c %d", (int)(intptr_t)testC, (int)(intptr_t)testw);
+            }
+            printf("\n\n Left Child \n");
+            p = huffTree_root(huffArr[n-1]);
+            p = huffTree_leftChild(huffArr[n-1],p);
+            if (huffTree_hasLeftChild(huffArr[n-1],p)){
+                p = huffTree_leftChild(huffArr[n-1],p);
+                data testC = huffTree_inspectCharacter(huffArr[n-1],p);
+                data testw = huffTree_inspectWeight(huffArr[n-1],p);
+                printf("Left %c %d", (int)(intptr_t)testC, (int)(intptr_t)testw);
+            }
+
+            p = huffTree_root(huffArr[n-1]);
+            p = huffTree_leftChild(huffArr[n-1],p);
+
+            if (huffTree_hasRightChild(huffArr[n-1],p)){
+                p = huffTree_rightChild(huffArr[n-1],p);
+                data testC = huffTree_inspectCharacter(huffArr[n-1],p);
+                data testw = huffTree_inspectWeight(huffArr[n-1],p);
                 printf("Right %c %d", (int)(intptr_t)testC, (int)(intptr_t)testw);
             }
     printf("\n\n");
-    for(int x= 0; x<size; x++){
-        printf("%c %d ", (char)(intptr_t)huffArr[x]->root->character, (int)(intptr_t)huffArr[x]->root->weight);
-    }
-    for(int x= 0; x<n-1; x++){
 
-        if (huffArr[x]->root!=NULL){
+    //for(int x= 0; x<2; x++){
+
+
+    //    if (huffArr[x]->root!=NULL)
+            //free(huffArr[x]->root);
+
         //huffTree_deleteNode(huffArr[x]->root);
-        }
-    }
+        //    huffTree_free(huffArr[x]);
 
-    return huffArr[n];
+    //}
+
+    return huffArr[n-1];
 }
 
 
@@ -266,13 +262,26 @@ int main (void){
     FILE *fp = fopen("test.txt","r");
     HuffIntArrayTable *h = calloc(1,sizeof(HuffIntArrayTable));
     readTextToArray(h,fp);
+//        printf ("\nOSORTERAT\n");
+//    for (int i= 0; i<h->arrNumber; i++){
+//        printf("%c %d\n", (char)h->array[i][0], h->array[i][1]);
+//    }
     treesortHuffArr(h,h->arrNumber);
+//        printf ("\nSORTERAT\n");
+//    for(int i= 0; i<h->arrNumber; i++){
+//        printf("%c %d\n", (char)h->array[i][0], h->array[i][1]);
+//    }
     huff_tree *huff = huffEncode(h);
     fclose(fp);
     free(h);
-    huffTree_free(huff);
-
-
+    //huffTree_free(huff);
+    //free(huff);
+//    if(huff->freeFunc && huffTree_hasCharacter(huff,huff->root))
+//        huff->freeFunc(huff->root->character);
+//    if(huff->freeFunc && huffTree_hasWeight(huff,huff->root))
+//        huff->freeFunc(huff->root->weight);
+//    free(huff->root);
+//    free(huff);
     return 0;
 
 
